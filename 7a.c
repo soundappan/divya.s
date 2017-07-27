3ad1bc93423200bc9c25a31fdf65f97f3baffe4e
soundappan <divyagovind23@gmail.com>
2017-07-27 14:57:52 +0530

    Create 7a.c

---
+++ 7a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+clrscr();
+printf("\n enter the value");
+scanf("%d",&n);
+if(n=='a')||(n=='e')||(n=='i')||(n=='o')||(n=='u')
+{
+printf("\n vowel");
+}
+else
+{
+printf("\n not a vowel");
+}
