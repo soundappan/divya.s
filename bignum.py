c60bc8069dc7137debdf28c86d3787f8c4fde623
Divya <divyagovind23@gmail.com>
2018-02-23 12:36:54 +0530

    Create bignum.py

---
+++ bignum.py
@@ -0,0 +1,8 @@
+ch = input("Enter any character: ")
+   if ch == '0':
+		break
+	else:
+	if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+			print(ch, "is an alphabet.\n")
+	else:
+			print(ch, "is not an alphabet.\n")
